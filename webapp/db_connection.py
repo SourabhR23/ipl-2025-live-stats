bf2103cba5231e04a008c7f187c49545ce1efed3
SourabhR23 <rasal.sourabh23@gmail.com>
2025-04-29 13:28:51 +0530

    Update files to solve error

--- webapp/db_connection.py
+++ webapp/db_connection.py
@@ -1,5 +1,8 @@
 import os
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
 
 MYSQL_USER = os.getenv("DB_USER")
 MYSQL_PASSWORD = os.getenv("DB_PASSWORD")
@@ -9,3 +12,5 @@ MYSQL_DATABASE = os.getenv("DB_NAME")
 def get_engine():
     db_url = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE}"
     return create_engine(db_url)
+
+
--- webapp/queries.py
+++ webapp/queries.py
@@ -1,8 +1,8 @@
 def get_all_matches():
-    return "SELECT * FROM IPL_Match_List ORDER BY date;"
+    return "SELECT * FROM IPL_Match_List ORDER BY match_date;"
 
 def get_batting_scorecard(match_id):
-    return f"SELECT * FROM batting_scorecard WHERE match_id = '{match_id}';"
+    return f"SELECT * FROM batting_df WHERE match_id = '{match_id}';"
 
 def get_bowling_scorecard(match_id):
-    return f"SELECT * FROM bowling_scorecard WHERE match_id = '{match_id}';"
+    return f"SELECT * FROM bowling_df WHERE match_id = '{match_id}';"
